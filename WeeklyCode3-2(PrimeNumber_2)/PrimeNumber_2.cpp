394f85d513c010d824f51cc7cef23f1186634f40
Attaphon <plianattaphon@gmail.com>
2021-08-22 13:15:26 +0700

    Add description

--- WeeklyCode3-2(PrimeNumber_2)/PrimeNumber_2.cpp
+++ WeeklyCode3-2(PrimeNumber_2)/PrimeNumber_2.cpp
@@ -1,13 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 int main() {
-	int num, count = 2;
+	int num, count = 2; // เริ่มที่ 2 เพราะ ต้องนับเลข 1 กับ ตัวมันเองด้วย
 	printf("Enter Number : ");
 	scanf("%d", &num);
 	while (count <= num && num % count > 0) {
 		count++;
 	}
-	if (count == num) {
+	if (count == num) { // ถ้าไม่เริ่มที่ 2 Prime Number (count จะไม่เท่ากับ num) 
 		printf("Number %d is a prime number.", num);
 	}
 	else {
