5e25d806f1bd383a816b95d5fc6e84e01d9a9f01
Attaphon <plianattaphon@gmail.com>
2021-08-22 14:24:35 +0700

    Correct the solution

--- WeeklyCode3-2(PrimeNumber_2)/PrimeNumber_2.cpp
+++ WeeklyCode3-2(PrimeNumber_2)/PrimeNumber_2.cpp
@@ -2,16 +2,16 @@
 #include<stdio.h>
 int main() {
 	int num, count = 2; // count begins at 2 because it must count at 1 and itself.
-	printf("Enter Number : ");
+	printf("Input a number : ");
 	scanf("%d", &num);
 	while (count <= num && num % count > 0) {
 		count++;
 	}
 	if (count == num) { // if count does't begin at 2 ,Prime number will not shown (count not equal to num).
-		printf("Number %d is a prime number.", num);
+		printf("%d is a prime number.", num);
 	}
 	else {
-		printf("Number %d is not a prime number.", num);
+		printf("%d is not a prime number.", num);
 	}
 	return 0;
 }
